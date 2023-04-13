475a6da5342629eda46e27077c15c4a818fe4858
basel2053 <baselsalah2053@gmail.com>
2023-04-13 15:27:27 +0200

    portfolios section

--- src/components/sliders/ProjcetSlider.tsx
+++ src/components/sliders/ProjcetSlider.tsx
@@ -17,10 +17,11 @@ const ProjectSlider = () => {
       pagination={{
         clickable: true,
       }}
+      navigation={true}
       modules={[Autoplay, Pagination, Navigation]}
       loop={true}
       autoplay={{
-        delay: 2500,
+        delay: 3500,
         disableOnInteraction: false,
       }}
       className="mySwiper project-slider"
@@ -28,37 +29,273 @@ const ProjectSlider = () => {
       <SwiperSlide>
         <Box
           sx={{
-            boxShadow: '0px 0px 15px rgba(0,0,0,0.05)',
             borderRadius: '20px',
             width: 460,
-            transition: '0.3s all',
-            ':hover': { opacity: 0.75 },
+            cursor: 'pointer',
           }}
         >
-          <img src="/images/portfolio-01.jpg" width="460" style={{ borderRadius: '20px' }} />
-          <Box sx={{ position: 'absolute', backgroundImage: `url(/images/hover-bg.png)` }}></Box>
+          <Box className="portfolio-container" sx={{ position: 'relative', overflow: 'hidden' }}>
+            <img src="/images/portfolio-01.jpg" width="460" style={{ borderRadius: '20px' }} />
+            <Box
+              sx={{
+                backgroundImage: 'url(/images/hover-bg.png)',
+                position: 'absolute',
+                backgroundRepeat: 'no-repeat',
+                color: '#fff',
+                zIndex: 20,
+                bottom: 0,
+                right: 10,
+                px: 8,
+                pt: 14,
+                pb: 2,
+                transform: 'translateY(216px)',
+                textAlign: 'right',
+              }}
+            >
+              <Typography variant="h5" fontWeight="700">
+                First Project
+              </Typography>
+              <p>Plot Listing</p>
+            </Box>
+          </Box>
         </Box>
       </SwiperSlide>
       <SwiperSlide>
         <Box
           sx={{
-            boxShadow: '0px 0px 15px rgba(0,0,0,0.05)',
             borderRadius: '20px',
             width: 460,
+            cursor: 'pointer',
           }}
         >
-          <img src="/images/portfolio-02.jpg" width="460" style={{ borderRadius: '20px' }} />
+          <Box className="portfolio-container" sx={{ position: 'relative', overflow: 'hidden' }}>
+            <img src="/images/portfolio-02.jpg" width="460" style={{ borderRadius: '20px' }} />
+            <Box
+              sx={{
+                backgroundImage: 'url(/images/hover-bg.png)',
+                position: 'absolute',
+                backgroundRepeat: 'no-repeat',
+                color: '#fff',
+                zIndex: 20,
+                bottom: 0,
+                right: 10,
+                px: 8,
+                pt: 14,
+                pb: 2,
+                transform: 'translateY(216px)',
+                textAlign: 'right',
+              }}
+            >
+              <Typography variant="h5" fontWeight="700">
+                Project Two
+              </Typography>
+              <p>SEO & Marketing</p>
+            </Box>
+          </Box>
         </Box>
       </SwiperSlide>
       <SwiperSlide>
         <Box
           sx={{
-            boxShadow: '0px 0px 15px rgba(0,0,0,0.05)',
             borderRadius: '20px',
             width: 460,
+            cursor: 'pointer',
           }}
         >
-          <img src="/images/portfolio-03.jpg" width="460" style={{ borderRadius: '20px' }} />
+          <Box className="portfolio-container" sx={{ position: 'relative', overflow: 'hidden' }}>
+            <img src="/images/portfolio-03.jpg" width="460" style={{ borderRadius: '20px' }} />
+            <Box
+              sx={{
+                backgroundImage: 'url(/images/hover-bg.png)',
+                position: 'absolute',
+                backgroundRepeat: 'no-repeat',
+                color: '#fff',
+                zIndex: 20,
+                bottom: 0,
+                right: 10,
+                px: 8,
+                pt: 14,
+                pb: 2,
+                transform: 'translateY(216px)',
+                textAlign: 'right',
+              }}
+            >
+              <Typography variant="h5" fontWeight="700">
+                Third Project
+              </Typography>
+              <p>Space Dynamic SEO</p>
+            </Box>
+          </Box>
+        </Box>
+      </SwiperSlide>
+      <SwiperSlide>
+        <Box
+          sx={{
+            borderRadius: '20px',
+            width: 460,
+            cursor: 'pointer',
+          }}
+        >
+          <Box className="portfolio-container" sx={{ position: 'relative', overflow: 'hidden' }}>
+            <img src="/images/portfolio-04.jpg" width="460" style={{ borderRadius: '20px' }} />
+            <Box
+              sx={{
+                backgroundImage: 'url(/images/hover-bg.png)',
+                position: 'absolute',
+                backgroundRepeat: 'no-repeat',
+                color: '#fff',
+                zIndex: 20,
+                bottom: 0,
+                right: 10,
+                px: 8,
+                pt: 14,
+                pb: 2,
+                transform: 'translateY(216px)',
+                textAlign: 'right',
+              }}
+            >
+              <Typography variant="h5" fontWeight="700">
+                Project Four
+              </Typography>
+              <p>Website Marketing</p>
+            </Box>
+          </Box>
+        </Box>
+      </SwiperSlide>
+      <SwiperSlide>
+        <Box
+          sx={{
+            borderRadius: '20px',
+            width: 460,
+            cursor: 'pointer',
+          }}
+        >
+          <Box className="portfolio-container" sx={{ position: 'relative', overflow: 'hidden' }}>
+            <img src="/images/portfolio-01.jpg" width="460" style={{ borderRadius: '20px' }} />
+            <Box
+              sx={{
+                backgroundImage: 'url(/images/hover-bg.png)',
+                position: 'absolute',
+                backgroundRepeat: 'no-repeat',
+                color: '#fff',
+                zIndex: 20,
+                bottom: 0,
+                right: 10,
+                px: 8,
+                pt: 14,
+                pb: 2,
+                transform: 'translateY(216px)',
+                textAlign: 'right',
+              }}
+            >
+              <Typography variant="h5" fontWeight="700">
+                Fifth Project
+              </Typography>
+              <p>Digital Assets</p>
+            </Box>
+          </Box>
+        </Box>
+      </SwiperSlide>
+      <SwiperSlide>
+        <Box
+          sx={{
+            borderRadius: '20px',
+            width: 460,
+            cursor: 'pointer',
+          }}
+        >
+          <Box className="portfolio-container" sx={{ position: 'relative', overflow: 'hidden' }}>
+            <img src="/images/portfolio-02.jpg" width="460" style={{ borderRadius: '20px' }} />
+            <Box
+              sx={{
+                backgroundImage: 'url(/images/hover-bg.png)',
+                position: 'absolute',
+                backgroundRepeat: 'no-repeat',
+                color: '#fff',
+                zIndex: 20,
+                bottom: 0,
+                right: 10,
+                px: 8,
+                pt: 14,
+                pb: 2,
+                transform: 'translateY(216px)',
+                textAlign: 'right',
+              }}
+            >
+              <Typography variant="h5" fontWeight="700">
+                Sixth Project
+              </Typography>
+              <p>SEO & Marketing</p>
+            </Box>
+          </Box>
+        </Box>
+      </SwiperSlide>
+      <SwiperSlide>
+        <Box
+          sx={{
+            borderRadius: '20px',
+            width: 460,
+            cursor: 'pointer',
+          }}
+        >
+          <Box className="portfolio-container" sx={{ position: 'relative', overflow: 'hidden' }}>
+            <img src="/images/portfolio-03.jpg" width="460" style={{ borderRadius: '20px' }} />
+            <Box
+              sx={{
+                backgroundImage: 'url(/images/hover-bg.png)',
+                position: 'absolute',
+                backgroundRepeat: 'no-repeat',
+                color: '#fff',
+                zIndex: 20,
+                bottom: 0,
+                right: 10,
+                px: 8,
+                pt: 14,
+                pb: 2,
+                transform: 'translateY(216px)',
+                textAlign: 'right',
+              }}
+            >
+              <Typography variant="h5" fontWeight="700">
+                7th Project
+              </Typography>
+              <p>SEO & Marketing</p>
+            </Box>
+          </Box>
+        </Box>
+      </SwiperSlide>
+      <SwiperSlide>
+        <Box
+          sx={{
+            borderRadius: '20px',
+            width: 460,
+            cursor: 'pointer',
+          }}
+        >
+          <Box className="portfolio-container" sx={{ position: 'relative', overflow: 'hidden' }}>
+            <img src="/images/portfolio-04.jpg" width="460" style={{ borderRadius: '20px' }} />
+            <Box
+              sx={{
+                backgroundImage: 'url(/images/hover-bg.png)',
+                position: 'absolute',
+                backgroundRepeat: 'no-repeat',
+                color: '#fff',
+                zIndex: 20,
+                bottom: 0,
+                right: 10,
+                px: 8,
+                pt: 14,
+                pb: 2,
+                transform: 'translateY(216px)',
+                textAlign: 'right',
+              }}
+            >
+              <Typography variant="h5" fontWeight="700">
+                8th Project
+              </Typography>
+              <p>SEO & Marketing</p>
+            </Box>
+          </Box>
         </Box>
       </SwiperSlide>
     </Swiper>
